7ce923fcd4620920631e0ec92423ab0fb05b6e9d
Lewis_Tyler_ART2210 <tlewi44@lsu.edu>
2019-09-21 23:34:06 -0500

    update

--- testCoding/sketch.js
+++ testCoding/sketch.js
@@ -1,7 +1,27 @@
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+ 
+
 }
 
 function draw() {
-  // put drawing code here
+ 
+  strokeWeight(4);
+  fill(255, 204, 0);
+  ellipse(windowWidth/3, windowHeight/2, 100, 55);
+  ellipse(windowWidth/2, windowHeight/3, 100, 55);
+ 
+  strokeWeight(1);
+  fill(255, 20, 0);
+  ellipse(windowWidth/2, windowHeight/2, 100, 55);
+
+
+ 
+  function windowResized(){
+    resizeCanvas(windowWidth,windowHeight);
+  
+ }
+ 
+
+
 }
