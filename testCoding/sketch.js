e6a13b6b944114f546e058a85f8ff400825f7a77
Lewis_Tyler_ART2210 <tlewi44@lsu.edu>
2019-09-22 10:15:46 -0500

    up

--- testCoding/sketch.js
+++ testCoding/sketch.js
@@ -10,8 +10,7 @@ function draw() {
  
   strokeWeight(4);
   fill(255, 204, 0);
-  arc(50, 55, 50, 50, 0, HALF_PI);
-  noFill()
+
  
   strokeWeight(1);
   fill(255, 20, 0);
@@ -27,6 +26,34 @@ function draw() {
 
  line(30, 20, 85, 75)
  stroke(30, 30, 45, 45)
+
+
+void setup() 
+  size(200, 200);
+  background(255);
+  smooth();
+
+  noFill();
+  stroke(0);
+  beginShape();
+  curveVertex(40, 40); // the first control point
+  curveVertex(40, 40); // is also the start point of curve
+  curveVertex(80, 60);
+  curveVertex(100, 100);
+  curveVertex(60, 120);
+  curveVertex(50, 150); // the last point of curve
+  curveVertex(50, 150); // is also the last control point
+  endShape();
+
+  // Use the array to keep the code shorter;
+  // you already know how to draw ellipses!
+  fill(255, 0, 0);
+  noStroke();
+  for (int i = 0; i < coords.length; i += 2) {
+    ellipse(coords[i], coords[i + 1], 3, 3);
+  }
+}
+
 
 
 }
