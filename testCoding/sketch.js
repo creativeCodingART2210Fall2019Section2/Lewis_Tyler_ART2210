cb9b4cb8453dd0905bc89c13d8b5e03a142c70cd
Lewis_Tyler_ART2210 <tlewi44@lsu.edu>
2019-09-22 22:32:11 -0500

    update

--- testCoding/sketch.js
+++ testCoding/sketch.js
@@ -1,59 +1,35 @@
 function setup() {
 
-  createCanvas(windowWidth,windowHeight);
-
-
-
+  createCanvas(850,850);
  
 
 }
 
 function draw() {
  
-  createCanvas(windowWidth,windowHeight);
-
-
-  noFill();
-  stroke(120);
-  beginShape();
-  curveVertex(240, 127); // the first control point
-  curveVertex(240, 127); // is also the start point of curve
-  curveVertex(283, 249);
-  curveVertex(333, 266);
-  curveVertex(407, 161);
-  curveVertex(453, 140); // the last point of curve
-  curveVertex(453, 140); // is also the last control point
-  endShape();
-
-  // Use the array to keep the code shorter;
-  // you already know how to draw ellipses!
-  fill(255, 0, 0);
-  noStroke();
-
-
-  strokeWeight(4);
-  fill(255, 204, 0);
-
- 
-  strokeWeight(1);
-  fill(255, 20, 0);
-  ellipse(windowWidth/2, windowHeight/2, 100, 55);
-
-
- 
-  function windowResized(){
-    resizeCanvas(windowWidth,windowHeight);
   
- }
+
+  strokeWeight(10);
+  line (575, 355, 655, 250);
+  strokeWeight(10);
+  line (445, 250, 460, 460);
+  strokeWeight(10);
+  line (675, 735, 560, 555);
+  strokeWeight(10);
+  line (580, 160, 525, 150);
+
+  circle(550, 200, 50)
+  circle (735, 275, 50)
+arc (759, 759, 450, 330, 0, HALF_PI)
+
+line (620, 500, 750, 375);
+
+  strokeWeight(10)
+  line(715, 215, 775, 250);
+
+line (825, 480, 825, 230)
 
 
-beginShape();
-vertex(450)
-endShape();
-
-
-
- line(30, 20, 85, 75)
- stroke(30, 30, 45, 45)
+noFill()
 
 }
