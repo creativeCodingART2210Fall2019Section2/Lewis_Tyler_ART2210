d0633a23d0658c1c97a2b631bd11412e9a7715ca
Lewis_Tyler_ART2210 <tlewi44@lsu.edu>
2019-09-23 12:03:42 -0500

    up

---
+++ testCoding/sketch.js
@@ -0,0 +1,35 @@
+function setup() {
+
+  createCanvas(850,850);
+ 
+
+}
+
+function draw() {
+ 
+  
+
+  strokeWeight(10);
+  line (575, 355, 655, 250);
+  strokeWeight(10);
+  line (445, 250, 460, 460);
+  strokeWeight(10);
+  line (675, 735, 560, 555);
+  strokeWeight(10);
+  line (580, 160, 525, 150);
+
+  circle(550, 200, 50)
+  circle (735, 275, 50)
+arc (759, 759, 450, 330, 0, HALF_PI)
+
+line (620, 500, 750, 375);
+
+  strokeWeight(10)
+  line(715, 215, 775, 250);
+
+line (825, 480, 825, 230)
+
+
+noFill()
+
+}
